f68958084c71a495327ff867d2a7752f068123a0
Jaweria B <bjaweria509@gmail.com>
2024-03-06 15:16:03 +0500

    removed home page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
+
 import NotFound from "pages/NotFound";
 import Homefull from "pages/Homefull";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    // { path: "/", element: <Home /> },
     { path: "*", element: <NotFound /> },
-    // {
-    //   path: "homefull",
-    //   element: <Homefull />,
-    // },
-    {
-      path: "/",
-      element: <Homefull />,
-    }
+    { path: "/", element: <Homefull />,}
   ]);
 
   return element;
